55d524bd53a9486d2040567604646d7f3236dfd8
iamtahmad1 <iamtahma1@gmail.com>
2023-10-03 20:15:25 +0530

    update

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Replace with your desired AMI ID
+  ami           = "ami-067c21fb1979f0b27" # Replace with your desired AMI ID
   instance_type = "t2.micro"             # Replace with your desired instance type
    tags = {
     Name = "MyEC2Instance"
